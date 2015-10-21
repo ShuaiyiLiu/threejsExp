6b3d48a42aba6f2f7072bd9021771afb96dbf78f
ShuaiyiLiu <davidliu.thu@gmail.com>
2015-10-21 18:11:36 +0800

    add stats and loading bar

--- js/heart.js
+++ js/heart.js
@@ -1,8 +1,11 @@
 var camera, scene, renderer;
+var cameraControls;
 var mesh;
+var mouseX = 0, mouseY = 0;
+
+var stats;
 
 init();
-animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera(45, 
@@ -14,6 +17,17 @@ function init() {
 
     var loader = new THREE.STLLoader();
 
+    function onProgress(request) {
+        var bar = 250, 
+            total = request.total,
+            loaded = request.loaded;
+        if (total) {
+            bar = Math.floor(bar * loaded / total);
+        }
+        console.info(bar);
+        $( "bar" ).style.width = bar + "px";
+    }
+
     loader.load('./obj/heart_n170111.stl', function(geometry) {
         var material = new THREE.MeshPhongMaterial({ 
             color: 0xff5533, specular: 0x111111, shininess: 200 } );
@@ -22,32 +36,52 @@ function init() {
 	    mesh.receiveShadow = true;
 	    scene.add( mesh );
         
+        // renderer
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
         // light
         var spotLight = new THREE.SpotLight(0xffffff);
         spotLight.position.set(-40, 60, -10);
         scene.add(spotLight);
 
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-
         document.body.appendChild(renderer.domElement);
+        //document.addEventListener('mousemove', onDocumentMouseMove, false);
         
-        window.addEventListener('resize', onWindowResize, false);
-    });
+        stats = new Stats();
+        stats.domElement.style.position = 'absolute';
+        stats.domElement.style.top = '0px';
+
+        document.body.appendChild(stats.domElement);
+
+         render();
+    }, onProgress);
+}
+
+function $(id) {
+    return document.getElementById(id);
+}
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - window.innerWidth / 2);
+    mouseY = (event.clientY - window.innerHeight / 2);
 }
 
 function onWindowResize() {
     camera.aspect = window.innerWidht / window.innerHeight;
     camera.updateProjectionMatrix();
+
 	renderer.setSize( window.innerWidth, window.innerHeight );
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-    
-    mesh.rotation.x += 0.005;
-    mesh.rotation.y += 0.01;
-    mesh.rotation.z += 0.005;
+function render() {
+    requestAnimationFrame(render);
 
+    stats.update();
+
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
+    
     renderer.render(scene, camera);
 }
